a4c62d94c234bca4a344c35832a0a9b67f48e5ef
Ben Hong <bencodezen@gmail.com>
2015-08-28 22:38:35 -0400

    Chore: Add Yelp API values for future use

--- app/scripts/map-engine.js
+++ app/scripts/map-engine.js
@@ -69,3 +69,8 @@ function initAutocomplete() {
     map.fitBounds(bounds);
   });
 }
+
+var consumerKey = 'VQMqc69WoO0Lm05nPszZBQ',
+    consumerSecret = 'CG0B-0CycMpoGSXQh2wQgaTq5wA',
+    token = 'jsHKoLeXXYXSic1KCUIu7g-vG2rm2AKv',
+    tokenSecret = 'CTp7AmbVye8sch3DrpG3mwLYMlg';
