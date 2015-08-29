2897be64592053c4a6ec329a069d10bd97b6b4e8
Ben Hong <bencodezen@gmail.com>
2015-08-29 18:23:21 -0400

    Feature: Add Google Map feature to the application

--- app/scripts/map-engine.js
+++ app/scripts/map-engine.js
@@ -1,3 +1,36 @@
+var map;
+
+// Function to create Google Map
+function initializeMap() {
+
+  /*
+   * Set starting location:
+   * Chinatown, San Francisco
+   */
+  var startLocation = new google.maps.LatLng(37.794612, -122.407861);
+
+  /*
+   * Set mapOptions for Google Maps
+   * Documentation can be found here
+   * http://bit.ly/1ItNfAl
+   */
+  var mapOptions = {
+    zoom: 16,
+    zoomControl: true,
+    center: startLocation,
+    disableDefaultUI: true
+  };
+
+  /* 
+   * Instantiate Google Map with the 
+   * above configurations 
+   */ 
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+};
+
+// Create map on page 
+initializeMap();
+
 /* 
  * Yelp Keys - Disabled Until Needed 
  *
