27956adbf3d60b33dada9d2326df936eac8024fa
Ben Hong <bencodezen@gmail.com>
2015-08-30 23:56:14 -0400

    Reset: Changing approach on how Knockout will be implemented

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -1,28 +1,10 @@
 /*
  * Overall View Model for this app
  */
-function yelpDataModel() {
+function yelpDataViewModel() {
 	// Assign 'this' to 'self' to avoid confusion in JS reference
 	var self = this;
 
-	// Array to contain all yelpBusinesses
-	self.businesses = [];
-
-	// Operations
-	self.addBusiness = function(name, lat, lng) {
-		self.businesses.push(new yelpBusiness(name, lat, lng));
-	}
-}
-
-/* 
- * Yelp Business Object
- */
-function yelpBusiness(name, lat, lng) {
-	// Assign 'this' to 'self' to avoid confusion in JS reference
-	var self = this;
-	self.name = name;
-	self.lat = lat;
-	self.lng = lng;
 }
 
 // Activate Knockout.js on page
