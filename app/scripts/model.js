b6bd80860c20c6cc537a5a1ad96fe5739fa68ebf
Ben Hong <bencodezen@gmail.com>
2015-08-31 00:07:50 -0400

    Feature: Tie search term to yelpDataViewModel

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -5,7 +5,9 @@ function yelpDataViewModel() {
 	// Assign 'this' to 'self' to avoid confusion in JS reference
 	var self = this;
 
+	// Have Knockout.js observe for changes in the search term
+	self.searchTerm = ko.observable("sushi");
 }
 
 // Activate Knockout.js on page
-ko.applyBindings(new yelpViewModel());
+ko.applyBindings(new yelpDataViewModel());
