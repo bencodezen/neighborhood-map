1e8635396f36748ef8ed28916e1198a238f9d6c1
Ben Hong <bencodezen@gmail.com>
2015-09-04 09:07:49 -0400

    Config - Add Gulp prefixer task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp'),
 		browserSync = require('browser-sync'),
+		autoprefixer = require('gulp-autoprefixer'),
 		reload = browserSync.reload;
+
+gulp.task('uglify', function() {
+	return gulp.src('app/styles/styles.css')
+		.pipe(autoprefixer({
+			browsers: ['last 4 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('app/dist/'));
+});
 
 gulp.task('default', function() {
 	browserSync({
